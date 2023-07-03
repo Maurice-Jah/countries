7e81b9526cbee9d66a8986eb5f83c554167313f4
Maurice-Jah <mauricejahwizwid@gmail.com>
2023-07-03 22:53:04 +0100

    Add and remove image after 2sec

--- script.js
+++ script.js
@@ -243,3 +243,58 @@ const whereAmI = function () {
 };
 
 btn.addEventListener('click', whereAmI);
+
+// Coding Challenge ------ make image to appear and disappear after 2 seconds
+
+const wait = function (secs) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, secs * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-3.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 3 loaded');
+    return wait(2);
+  })
+  .then(() => (currentImg.style.display = 'none'))
+
+  .catch(err => console.error(err));
